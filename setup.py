fe7f20b2ebd1e32c557cdc9d3f52006f4778f4ff
Philippe Virouleau <philippe.virouleau@imag.fr>
2016-06-02 00:07:25 +0200

    Upload package data too.

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(name='wrc',
-      version='1.0.0a1',
+      version='1.0.0a2',
       description='Tool to build and perform checks on WCA Regulations and Guidelines',
       url='http://github.com/viroulep/wca-regulations-compiler',
       author='Philippe Virouleau',
       author_email='philippe.44@gmail.com',
       license='GPLv3',
       packages=find_packages(),
+      package_data={
+          'wrc': ['data/*'],
+      },
       entry_points={
           'console_scripts': ['wrc=wrc.wrc:run'],
       },
