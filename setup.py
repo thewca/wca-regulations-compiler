2557eb5aa8c597b939c27aa068197e667c50d857
Philippe Virouleau <philippe.virouleau@imag.fr>
2016-06-09 00:31:37 +0200

    Move url to 'cubing'.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name='wrc',
-      version='1.0.2',
+      version='1.0.3',
       description='Tool to build and perform checks on WCA Regulations and Guidelines',
-      url='http://github.com/viroulep/wca-regulations-compiler',
+      url='http://github.com/cubing/wca-regulations-compiler',
       author='Philippe Virouleau',
       author_email='philippe.44@gmail.com',
       license='GPLv3',
