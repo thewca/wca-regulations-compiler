422e16d2c561a870da57996e6b97bcdbd92c16be
Nanush7 <nanusoft7@protonmail.com>
2024-01-27 21:24:43 -0300

    Hidden guidelines

--- wrc/codegen/cghtml.py
+++ wrc/codegen/cghtml.py
@@ -94,10 +94,13 @@ class WCADocumentHtml(CGDocument):
     def __init__(self, versionhash, language, pdf, merged=False):
         super(WCADocumentHtml, self).__init__(str)
         self.regset = set()
+        self.merged = merged
         if merged:
             self.urls = {'regulations': MERGED_URI, 'guidelines': MERGED_URI, 'pdf': pdf}
+            self.guideline_c_attr = ' class="hidden-guideline" hidden="true"'
         else:
             self.urls = {'regulations': './', 'guidelines': './guidelines.html', 'pdf': pdf}
+            self.guideline_c_attr = ''
         self.language = language
 
         is_translation = (language != "english")
@@ -117,7 +120,7 @@ class WCADocumentHtml(CGDocument):
                          '</h2></div></div>\n')
         self.label = ('<li><span class="{name} label label-default">{name}</span> '
                       '{text}</li>\n')
-        self.guideline = ('<li id="{i}"><a href="#{i}">{i}</a>) '
+        self.guideline = ('<li id="{i}"{c}><a href="#{i}">{i}</a>) '
                           '<span class="{label} label {linked}">'
                           '<a {attr}>{label}</a></span> {text}</li>\n')
         self.regulation = '<li id="{i}"><a href="#{i}">{i}</a>) {text}'
@@ -133,6 +136,8 @@ class WCADocumentHtml(CGDocument):
                                     isinstance(a_list[0], LabelDecl))
 
     def visitWCADocument(self, document):
+        if self.merged:
+            self.codegen += '<%= react_component("Regulations") %>'
         self.codegen += '<div class="container">'
         self.codegen += TITLE.format(title=document.title)
         self.codegen += VERSION.format(version=document.version,
@@ -218,6 +223,7 @@ class WCADocumentHtml(CGDocument):
         attr = link_attr if linked else anchor_attr
 
         self.codegen += self.guideline.format(i=guide.number,
+                                              c=self.guideline_c_attr,
                                               text=self.md2html(guide.text),
                                               label=guide.labelname,
                                               linked=label_class,
