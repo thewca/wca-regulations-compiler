d71ce1865a3b454da52ca406b537746b92f07cc4
Nanush7 <nanusoft7@protonmail.com>
2024-04-05 01:17:21 -0300

    Improvements

--- wrc/codegen/merger.py
+++ wrc/codegen/merger.py
@@ -12,7 +12,7 @@ class BadFormatError(Exception):
         super().__init__(message)
 
 
-def compare_article_numbers(num1: str, num2: str) -> bool:
+def article_number_is_lower(num1: str, num2: str) -> bool:
     """
     Returns True if num1 < num2, otherwise returns False.
     This is necessary because:
@@ -22,19 +22,17 @@ def compare_article_numbers(num1: str, num2: str) -> bool:
     is_digit_1 = num1.isdigit()
     is_digit_2 = num2.isdigit()
     if is_digit_1 and is_digit_2:
-        res = int(num1) < int(num2)
+        return int(num1) < int(num2)
     elif is_digit_1 and not is_digit_2:
         # Only num2 is a letter.
-        res = True
+        return True
     elif not is_digit_1 and not is_digit_2:
-        res = num1 < num2
-    else:
-        res = False
+        return num1 < num2
 
-    return res
+    return False
 
 
-def compare_reg_numbers(num1: list, num2: list) -> bool:
+def reg_number_is_lower(num1: list, num2: list) -> bool:
     """
     Returns True if num1 < num2, otherwise returns False.
     We need this for the same reasons we need compare_article_numbers().
@@ -48,7 +46,7 @@ def compare_reg_numbers(num1: list, num2: list) -> bool:
     return len(num1) < len(num2)
 
 
-def get_reg_number(reg):
+def get_reg_number(reg) -> list:
     if isinstance(reg, Guideline):
         reg = reg.number.split('+')[0]
         num = split_rule_number(reg)
@@ -71,9 +69,12 @@ def merge_ast(astreg, astguide, language_options):
         raise BadFormatError('NOTES_INDEX is incorrect')
     if not isinstance(astreg.sections[TOC_INDEX], TableOfContent):
         raise BadFormatError('TOC_INDEX is incorrect')
-    if not (hasattr(astreg.sections[FIRST_ARTICLE_INDEX], 'number') and astreg.sections[
-                FIRST_ARTICLE_INDEX].number == '1'):
-        raise BadFormatError('FIRST_ARTICLE_INDEX is incorrect.')
+    if not (hasattr(astreg.sections[FIRST_ARTICLE_INDEX], 'number') and
+            astreg.sections[FIRST_ARTICLE_INDEX].number == '1'):
+        raise BadFormatError('FIRST_ARTICLE_INDEX is incorrect for astreg')
+    if not (hasattr(astguide.sections[FIRST_ARTICLE_INDEX], 'number') and
+            astguide.sections[FIRST_ARTICLE_INDEX].number == '1'):
+        raise BadFormatError('FIRST_ARTICLE_INDEX is incorrect for astguide')
 
     # We take the astreg as the base, and then we add the guidelines in their corresponding places.
     ast_combined = astreg
@@ -85,23 +86,26 @@ def merge_ast(astreg, astguide, language_options):
     ast_combined.sections[NOTES_INDEX].content.append(
         [cont for cont in astguide.sections[NOTES_INDEX].content if language_options['regulations'] not in cont.title])
 
-    # Now we iterate over all the Articles found in astguide, starting from FIRST_ARTICLE_INDEX.
-    for guide_section in astguide.sections[FIRST_ARTICLE_INDEX:]:
+    # Now we iterate over all the Articles found in astguide, from FIRST_ARTICLE_INDEX until the end of the
+    # sections list.
+    guide_sections = astguide.sections[FIRST_ARTICLE_INDEX:]
+    for guide_section in guide_sections:
 
         stack = deepcopy(guide_section.content)
         reg_section = None
         # Find the associated section in the Regulations or add it if there isn't one.
-        for index, section in enumerate(ast_combined.sections[FIRST_ARTICLE_INDEX:]):
+        ast_combined_sections = ast_combined.sections[FIRST_ARTICLE_INDEX:]
+        for index, section in enumerate(ast_combined_sections):
             if not isinstance(section, Article):
                 # We could just continue here, but finding a non-Article section after FIRST_ARTICLE_INDEX
                 # is worth raising an exception.
-                raise BadFormatError('Section is not an Article.')
+                raise BadFormatError('Section is not an Article')
 
             if section.number == guide_section.number:
                 # We found the section!
                 reg_section = section
                 break
-            elif compare_article_numbers(guide_section.number, section.number):
+            elif article_number_is_lower(guide_section.number, section.number):
                 # This happens when an Article in the Guidelines doesn't have an associated article in the Regulations.
                 ast_combined.sections[TOC_INDEX].articles.insert(index, guide_section)
                 # Now we copy the Article from the Guidelines into the ast_combined and continue (empty stack).
@@ -135,11 +139,11 @@ def recursive_insert(root, guideline, guideline_num, stack) -> int:
         node_num = get_reg_number(node)
 
         # if len == len and node_num >= guideline_num.
-        if len(node_num) == len(guideline_num) and not compare_reg_numbers(node_num, guideline_num):
+        if len(node_num) == len(guideline_num) and not reg_number_is_lower(node_num, guideline_num):
             if node_num == guideline_num:
                 # The guideline goes below the current node.
                 inserted_index = index + 1
-            elif compare_reg_numbers(guideline_num, node_num):
+            elif reg_number_is_lower(guideline_num, node_num):
                 # We have len == len, but the iteration went over the number without finding a regulation with the same
                 # number. The guideline doesn't have an associated regulation, so we insert it before the current node.
                 inserted_index = index
